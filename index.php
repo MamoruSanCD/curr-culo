ef27f426fc28541e3fd066e3b5ae47fd253a06cb
MamoruSanCD <mamorusanstudio@gmail.com>
2025-07-22 23:47:45 -0300

    Criação do index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Matheus</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="foto"></div>
+            <div class="resumo"></div>
+            <div class="nome"></div>
+        </div>
+        <div class="aside"></div>
+        <div class="main"></div>
+        <div class="footer"></div>
+    </div>
+    
+</body>
+</html>
